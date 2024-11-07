d063752fc47a094fc1b1b042447dc3ea451fe64d
Mohamed Majdoub <mohmajdo@e1r8p3.1337.ma>
2024-11-07 22:50:04 +0100

    new commit

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,32 +6,47 @@
 /*   By: mohmajdo <mohmajdo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 00:03:32 by mohmajdo          #+#    #+#             */
-/*   Updated: 2024/11/07 17:07:29 by mohmajdo         ###   ########.fr       */
+/*   Updated: 2024/11/07 22:40:55 by mohmajdo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static void	ft_fill(unsigned char *p1, const unsigned char *p2,size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (p1 < p2)
+	{
+		i = 0;
+		while (i < n)
+		{
+			p1[i] = p2[i];
+			i++;
+		}
+	}
+	else
+	{
+		i = n;
+		while (i > 0)
+		{
+			p1[i - 1] = p2[i - 1];
+			i--;
+		}
+	}
+}
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	unsigned char	*p1;
-	unsigned char	*p2;
-	size_t			i;
+	const unsigned char	*p2;
 
-	i = 0;
 	p1 = (unsigned char *)dst;
-	p2 = (unsigned char *)src;
-	if (!dst || !src)
+	p2 = (const unsigned char *)src;
+	if (dst == NULL && src == NULL)
 		return (NULL);
-	if (p1 > p2)
-	{
-		while (n--)
-			p1[n] = p2[n];
-	}
-	else
-	{
-		while (i++ < n)
-			p1[i] = p2[i];
-	}
+	if (n == 0)
+		return (dst);
+	ft_fill(p1, p2, n);
 	return (dst);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mohmajdo <mohmajdo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 20:02:33 by mohmajdo          #+#    #+#             */
-/*   Updated: 2024/11/07 16:54:49 by mohmajdo         ###   ########.fr       */
+/*   Updated: 2024/11/07 22:49:27 by mohmajdo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,21 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dest_len;
 	size_t	src_len;
 
+	if ((dst == NULL || src == NULL) && dstsize == 0)
+		return (0);
 	dest_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	i = 0;
+	if (dstsize == 0)
+		return (src_len);
+
 	if (dstsize <= dest_len)
-		return (dest_len + src_len);
+		return (dstsize + src_len);
 	while (src[i] && (dest_len + i < dstsize - 1))
 	{
-		dst[i + dest_len] = src[i];
+		dst[dest_len + i] = src[i];
 		i++;
 	}
 	dst[dest_len + i] = '\0';
-	return (src_len + dest_len);
+	return (dest_len + src_len);
 }
